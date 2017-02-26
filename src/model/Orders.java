d2ce4da8990408b59f2ba0e46f9060feb9b8e3d2
thien0411 <thien.nguyen0411@gmail.com>
2017-02-26 15:27:53 -0600

    Add order model

--- src/model/Orders.java
+++ src/model/Orders.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Orders {
+import javax.servlet.http.HttpServletRequest;
 
+public class Orders {
+		private Double total;
+		private Integer orderNumber;
+		private String orderDatel;
+		
+		
+		
+		public Orders(Double total, Integer orderNumber, String orderDatel) {
+			super();
+			this.total = total;
+			this.orderNumber = orderNumber;
+			this.orderDatel = orderDatel;
+		}
+		public Double getTotal() {
+			return total;
+		}
+		public void setTotal(Double total) {
+			this.total = total;
+		}
+		public Integer getOrderNumber() {
+			return orderNumber;
+		}
+		public void setOrderNumber(Integer orderNumber) {
+			this.orderNumber = orderNumber;
+		}
+		public String getOrderDatel() {
+			return orderDatel;
+		}
+		public void setOrderDatel(String orderDatel) {
+			this.orderDatel = orderDatel;
+		}
+		
 }
